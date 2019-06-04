2a678dcb37c1bab11a33dc5c1b2078f06760b015
Mihret De Jong <mihret.dejong@gmail.com>
2019-06-04 17:09:18 -0600

    initial-commit

---
+++ exercise/hello-class/app.js
@@ -0,0 +1,28 @@
+
+/*var sayHello = function(){
+    console.log("hello class")
+}
+sayHello()
+
+function sayHello(){
+    console.log("hello class")
+}
+
+
+function sayHello(greeting,name){
+    console.log(greeting + " " + name); 
+    return greeting + name // stores information in the code vs console.log - states what you're doing
+    
+}
+sayHello("Hello","Fred!")
+sayHello("what's up", "danny")
+*/
+
+function sayHello(greeting,phrase, num){
+    console.log(greeting + phrase +" " + num + "?")
+}
+
+
+sayHello("Hello Fred", "how do you like being" ,27)
+
+
