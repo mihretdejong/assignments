ea98a1f1a1cb00e0d43f12ac34c36fa440856e29
Mihret De Jong <mihret.dejong@gmail.com>
2019-08-30 10:00:18 -0500

    polishtrivia

--- projects/trivia-react-project/src/Footer.js
+++ projects/trivia-react-project/src/Footer.js
@@ -4,9 +4,10 @@ import './style/footer.css'
 const Footer = () => {
     return (
         <div className="footer-div">
-            <a href="#"><div className="linkedin"/></a>
-            <a href="#"><div className="github"/></a>
-            <a href="#"><div className="twitter"/></a>
+            <a href="https://mihretdj.surge.sh"><div className="icons portfolio"/></a>
+            <a href="https://www.linkedin.com/in/mihret-de-jong-56911278/"><div className="icons linkedin"/></a>
+            <a href="https://github.com/mihretdejong/assignments/tree/master/projects/trivia-react-project"><div className="icons github"/></a>
+            <a href="https://twitter.com/DeMihret"><div className="icons twitter"/></a>
         </div>
     )
 }
